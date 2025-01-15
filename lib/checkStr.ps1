5dab7e1f3c79184716cf07ae96381ac10ce3a1ba
Justin Cooper <jcooper@chicousd.org>
2025-01-15 07:58:14 -0800

    clean up

--- lib/checkStr.ps1
+++
@@ -1,6 +0,0 @@
-function checkStr ($myString) {
- $phrases = 'drop table', 'truncate table', 'alter table', 'update', 'delete', 'create'
- foreach ($phrase in $phrases) {
-  if ($myString -match $phrase) { "Unsupported Phrase Detected: [$phrase]" }
- }
-}
