052db941746b9d83cbab775ba6ae5d91d1acbe2d
Justin Cooper <jcooper@chicousd.org>
2025-01-24 11:18:17 -0800

    fixed dc issue

--- sql/insert-otp.sql
+++ sql/insert-otp.sql
@@ -1,4 +1,4 @@
 INSERT INTO {0}
-(permId,stuNum,grade,siteCode,samid,staffEmail,otp,requestDate,source)
+(permId,stuNum,grade,siteCode,samid,staffEmail,otp,requestDate,source,dts)
 VALUES
-({1},{2},{3},{4},'{5}','{6}','{7}','{8}','{9}');
+({1},{2},{3},{4},'{5}','{6}','{7}','{8}','{9}',GETDATE());
